2e0459a4d41e45ed10a934696788d9dcbc654f9d
Abir Taheer <abir@taheer.me>
2020-05-09 17:25:04 -0400

    added code for checking ports

---
+++ index.js
@@ -0,0 +1,14 @@
+const portscanner = require('portscanner');
+const args = require('minimist')(process.argv.slice(2));
+
+const port = Number(args['port']);
+
+portscanner.checkPortStatus(port, '127.0.0.1', function (error, status) {
+	// Status is 'open' if currently in use or 'closed' if available
+	if(error){
+		console.error(error);
+		process.exit(1);
+	}
+
+	process.exit(status === 'open' ? 0 : 1);
+});
